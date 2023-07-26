6edc1c3c22c132c4804e4f5a9b7613d005c187f1
pauls3 <paulstanik3@gmail.com>
2023-07-25 23:14:56 -0700

    returning to default settings

--- code/models/C3DAVG/C3D_altered.py
+++ code/models/C3DAVG/C3D_altered.py
@@ -37,11 +37,11 @@ class C3D_altered(nn.Module):
 
         self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool4 = nn.MaxPool3d(kernel_size=(1, 1, 1), stride=(2, 2, 2))
+        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
         self.conv5a = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.conv5b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool5 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
+        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
 
         self.relu = nn.ReLU()
 
