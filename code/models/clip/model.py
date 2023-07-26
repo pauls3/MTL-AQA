68a8f98b82c0204d13aa057c91622c6e399844ed
pauls3 <paulstanik3@gmail.com>
2023-07-26 16:34:30 -0700

    increased padding to 6

--- code/models/clip/model.py
+++ code/models/clip/model.py
@@ -208,7 +208,7 @@ class VisionTransformer(nn.Module):
         super().__init__()
         self.input_resolution = input_resolution
         self.output_dim = output_dim
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False, padding=6)
 
         scale = width ** -0.5
         self.class_embedding = nn.Parameter(scale * torch.randn(width))
