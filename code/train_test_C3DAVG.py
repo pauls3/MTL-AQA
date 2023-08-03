f3d9eab8396050457dbf532fbbbf1210b4c3eda3
pauls3 <paulstanik3@gmail.com>
2023-08-03 08:12:31 -0700

    fixed torch.narrow params

--- code/train_test_C3DAVG.py
+++ code/train_test_C3DAVG.py
@@ -102,8 +102,8 @@ def train_phase(train_dataloader, optimizer, criterions, epoch):
             preprocessed = preprocess(clips_feats_avg_re_img).unsqueeze(0).cuda()
             
             print(true_captions.shape)
-            true_captions = torch.narrow(true_captions, 0, 1, 77)
-            true_captions_mask = torch.narrow(true_captions_mask, 0, 1, 77)
+            true_captions = torch.narrow(true_captions, 1, 0, 77)
+            true_captions_mask = torch.narrow(true_captions_mask, 1, 0, 77)
             print(true_captions.shape)
             # true_captions_mask_pad = torch.reshape(true_captions_mask, (4, 75))
             # true_captions_mask_pad_re = nn.functional.pad(true_captions_mask_pad, pad=(0,2))
