671892704408feb6095e50d4e6ec556945e54874
pauls3 <paulstanik3@gmail.com>
2023-08-10 08:58:02 -0700

    using differnt input for l1score

--- code/train_test_C3DAVG.py
+++ code/train_test_C3DAVG.py
@@ -140,7 +140,7 @@ def train_phase(train_dataloader, optimizer, criterions, epoch):
             print(clip_probs.size())
             print(clip_probs)
             print(true_final_score_clip.size())
-            loss_clip = criterion_clip(clip_probs, true_final_score_clip)
+            loss_clip = criterion_clip(clip_probs, true_final_score_temp)
             loss += loss_clip*0.01
 
         optimizer.zero_grad()
