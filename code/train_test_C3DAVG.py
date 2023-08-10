8b6520fec0be8d2f50fad262aa30e14c7d55b5a3
pauls3 <paulstanik3@gmail.com>
2023-08-09 19:02:47 -0700

    converting to int using astype(int)

--- code/train_test_C3DAVG.py
+++ code/train_test_C3DAVG.py
@@ -66,7 +66,8 @@ def train_phase(train_dataloader, optimizer, criterions, epoch):
         true_final_score = data['label_final_score'].unsqueeze_(1).type(torch.FloatTensor).cuda()
 
         tfs_np = true_final_score.cpu().numpy()
-        true_final_score_clip = int(tfs_np * 10000)
+        tfs_np = tfs_np * 10000
+        true_final_score_clip = tfs_np.astype(int)
 
         if with_dive_classification:
             true_postion = data['label_position'].cuda()
