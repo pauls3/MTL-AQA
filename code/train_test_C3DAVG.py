7db3cc86b717fab22038950e36c1f932ebfd35a7
pauls3 <paulstanik3@gmail.com>
2023-08-09 18:57:17 -0700

    converted true final score to np before converting to int

--- code/train_test_C3DAVG.py
+++ code/train_test_C3DAVG.py
@@ -64,7 +64,10 @@ def train_phase(train_dataloader, optimizer, criterions, epoch):
     iteration = 0
     for data in train_dataloader:
         true_final_score = data['label_final_score'].unsqueeze_(1).type(torch.FloatTensor).cuda()
-        true_final_score_clip = int(data['label_final_score'].unsqueeze_(1) * 10000)
+
+        tfs_np = true_final_score.numpy()
+
+        true_final_score_clip = int(true_final_score * 10000)
         if with_dive_classification:
             true_postion = data['label_position'].cuda()
             true_armstand = data['label_armstand'].cuda()
