973f44eb55486661aac238336f30367fc4def432
pauls3 <paulstanik3@gmail.com>
2023-08-09 18:58:51 -0700

    converting tensor to numpy using cpu

--- code/train_test_C3DAVG.py
+++ code/train_test_C3DAVG.py
@@ -65,9 +65,9 @@ def train_phase(train_dataloader, optimizer, criterions, epoch):
     for data in train_dataloader:
         true_final_score = data['label_final_score'].unsqueeze_(1).type(torch.FloatTensor).cuda()
 
-        tfs_np = true_final_score.numpy()
-
+        tfs_np = true_final_score.cpu().numpy()
         true_final_score_clip = int(true_final_score * 10000)
+
         if with_dive_classification:
             true_postion = data['label_position'].cuda()
             true_armstand = data['label_armstand'].cuda()
