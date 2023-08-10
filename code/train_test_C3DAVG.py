f6b5e100fe8ecddf84c813fd8161b14577c7cf16
pauls3 <paulstanik3@gmail.com>
2023-08-10 09:13:13 -0700

    squeezing probs for l1loss

--- code/train_test_C3DAVG.py
+++ code/train_test_C3DAVG.py
@@ -138,7 +138,10 @@ def train_phase(train_dataloader, optimizer, criterions, epoch):
 
             # true_final_score_clip = torch.reshape(true_final_score_clip, (1,3))
             clip_loss_gt = torch.squeeze(true_final_score_temp)
-            loss_clip = criterion_clip(clip_probs, clip_loss_gt)
+            clip_probs_sqz = torch.squeeze(clip_probs)
+            print(clip_probs_sqz.size())
+            print(clip_loss_gt.size())
+            loss_clip = criterion_clip(clip_probs_sqz, clip_loss_gt)
             loss += loss_clip*0.01
 
         optimizer.zero_grad()
